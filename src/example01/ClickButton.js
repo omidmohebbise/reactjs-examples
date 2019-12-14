da992c3c903aad8aa2e21c1d472d3f255ec63671
Omid Mohebbi <omidmohebbise@gmail.com>
2019-12-14 23:23:55 +0330

    Exammple 01: create component, interval, event example and ....

---
+++ src/example01/ClickButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function clickk(){
+    alert("fuck");
+}
+
+function ClickButtom(){
+    return <button onClick={clickk}>Click Me</button>
+}
+export default ClickButtom;
---
+++ src/example01/ComponentWithProbs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ComponentWithProbs(content){
+    return(
+    <div>
+        <h1>Probs:</h1>
+        <p>content</p>
+    </div>    
+    );
+}
+export default ComponentWithProbs;
