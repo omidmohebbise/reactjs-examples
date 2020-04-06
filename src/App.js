d496f7edcaebb45bc8e53e6a6082aaa537afbcb7
Omid Mohebbi <omid.mohebbi@rdsysco.com>
2020-04-06 15:30:20 +0430

    A column chart added to Project

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import './App.css';
 import Example01 from './example01/Example01';
 import Example02 from './example02/Example02';
 import PieExamples from './pie-charts/PieCharts';
+import ColumnChart from './column-chart/culumnchart';
+
 function App() {
+  
   return (
   //  <Example01></Example01>
   // <Example02></Example02>
-  <PieExamples></PieExamples>
+  // <PieExamples></PieExamples>
+  <ColumnChart></ColumnChart>
   );
 }
 
