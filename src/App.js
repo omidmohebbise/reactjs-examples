429753bb4ef0ef98e5a3338a548ba13a587b8c5d
Omid Mohebbi <omid.mohebbi@rdsysco.com>
2020-04-06 11:58:45 +0430

    add pie example to project :)

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './App.css';
 
 import Example01 from './example01/Example01';
 import Example02 from './example02/Example02';
+import PieExamples from './pie-charts/PieCharts';
 function App() {
   return (
   //  <Example01></Example01>
-  <Example02></Example02>
+  // <Example02></Example02>
+  <PieExamples></PieExamples>
   );
 }
 
